fd1527943c71e12957613282f5eda4daed5a4020
John Picker <john@ossistemes.com>
2024-07-17 18:31:12 +0200

    Finis to demo

--- App.js
+++ App.js
@@ -3,7 +3,12 @@ import {
   MultipleVerticalBars,
   PieChart,
 } from "d3-graphic";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { StatusBar } from "expo-status-bar";
+import {
+  NavigationContainer,
+  useNavigationContainerRef,
+} from "@react-navigation/native";
 import { useState } from "react";
 import {
   Button,
@@ -15,120 +20,199 @@ import {
 } from "react-native";
 
 const initialData = [
-  { nameProduct: "Apple", value: 100, id: 1 },
+  { nameProduct: "Apples", value: 100, id: 1 },
   { nameProduct: "Banana", value: 150, id: 2 },
   { nameProduct: "Cactus", value: 45, id: 3 },
-  { nameProduct: "Daiquiri", value: 169.48, id: 4 },
-  { nameProduct: "Element", value: 188.4, id: 5 },
-  { nameProduct: "Fruits", value: 90, id: 6 },
+  { nameProduct: "Dades", value: 169.48, id: 4 },
+  { nameProduct: "Guitar", value: 188.4, id: 5 },
+  { nameProduct: "Fruit", value: 90, id: 6 },
   { nameProduct: "Games", value: 25, id: 7 },
 ];
 
 export default function App() {
+  const navigationRef = useNavigationContainerRef();
+
+  return (
+    <SafeAreaView>
+      <ScrollView
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 5,
+          marginTop: 20,
+        }}
+      >
+        <NavigationContainer ref={navigationRef}>
+          <TabNavigator
+            tabs={[
+              {
+                title: "Horizontal Bars",
+                component: <GenericTab type={"horizontalBars"} />,
+              },
+              {
+                title: "Vertical Bars",
+                component: <GenericTab type={"verticalBars"} />,
+              },
+              {
+                title: "Pie Chart",
+                component: <GenericTab type={"pieChart"} />,
+              },
+            ]}
+            initialRouterName={"Horizontal Bars"}
+          />
+        </NavigationContainer>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const Tab = createMaterialTopTabNavigator();
+const TabNavigator = ({ tabs, initialRouterName }) => {
+  return (
+    <Tab.Navigator
+      initialRouteName={initialRouterName}
+      screenOptions={{
+        swipeEnabled: true,
+        tabBarItemStyle: { width: "auto" },
+        tabBarActiveTintColor: "#06bcee",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: { backgroundColor: "white" },
+        tabBarIndicatorStyle: { backgroundColor: "#06bcee", height: 2 },
+      }}
+    >
+      {tabs.map((tab, index) => (
+        <Tab.Screen key={index} name={tab.title}>
+          {() => tab.component}
+        </Tab.Screen>
+      ))}
+    </Tab.Navigator>
+  );
+};
+
+const GenericTab = ({ type }) => {
   const [data, setData] = useState(initialData);
 
   const changeData = () => {
     setData((prevState) =>
       prevState.map((row) => ({
         ...row,
-        value: Math.floor(Math.random() * 500),
+        value: Math.floor(Math.random() * 1500),
       }))
     );
   };
 
   return (
-    <SafeAreaView>
-      <ScrollView style={{ display: "flex", flexDirection: "column", gap: 5 }}>
-        <View style={styles.container}>
-          <StatusBar style="auto" />
-          <Text style={styles.title}>
-            Testing Library of chart created by JohnJLM 💻📊
-          </Text>
-          <Button
-            onPress={() => changeData()}
-            title="Change Data Values"
-            color="#06bcee"
-          />
-          <View style={styles.containerCard}>
-            {/* Without Scroll */}
-            <Text style={styles.title}>Testing MultipleHorizontalBars 📈</Text>
-            <View style={styles.containerHorizontalGraph}>
-              <MultipleHorizontalBars
-                data={data}
-                keyLabel={"nameProduct"}
-                keyValue={"value"}
-                primaryKey={"id"}
-                error={false}
-                loading={false}
-              />
-            </View>
-            {/* With Scroll */}
-            <Text style={styles.title}>Testing internal scroll 📈</Text>
-            <View style={[styles.containerHorizontalGraph, { height: 150 }]}>
-              <MultipleHorizontalBars
-                data={data}
-                keyLabel={"nameProduct"}
-                keyValue={"value"}
-                primaryKey={"id"}
-                error={false}
-                loading={false}
-              />
-            </View>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 2,
+        }}
+      >
+        <Text style={styles.title}>
+          Testing Library of chart created by JohnJLM 💻📊
+        </Text>
+        <Text style={styles.title}>Get a Dashboard in minuts ⌛</Text>
+      </View>
+      <Button
+        onPress={() => changeData()}
+        title="Change Data Values"
+        color="#06bcee"
+      />
+
+      {/* MultipleHorizontalBars */}
+      {type === "horizontalBars" && (
+        <View style={styles.containerCard}>
+          {/* Without Scroll */}
+          <Text style={styles.title}>MultipleHorizontalBars 📈</Text>
+          <View style={styles.containerHorizontalGraph}>
+            <MultipleHorizontalBars
+              data={data}
+              keyLabel={"nameProduct"}
+              keyValue={"value"}
+              primaryKey={"id"}
+              error={false}
+              loading={false}
+            />
           </View>
-          <View style={styles.containerCard}>
-            {/* Without Scroll */}
-            <Text style={styles.title}>Testing MultipleVerticalBars 📈</Text>
-            <View style={styles.containerVerticalGraph}>
-              <MultipleVerticalBars
-                data={data}
-                keyLabel={"nameProduct"}
-                keyValue={"value"}
-                primaryKey={"id"}
-                error={false}
-                loading={false}
-              />
-            </View>
-            {/* With Scroll */}
-            <Text style={styles.title}>Testing internal scroll 📈</Text>
-            <View style={[styles.containerVerticalGraph, { width: 180 }]}>
-              <MultipleVerticalBars
-                data={data}
-                keyLabel={"nameProduct"}
-                keyValue={"value"}
-                primaryKey={"id"}
-                error={false}
-                loading={false}
-              />
-            </View>
-          </View>
-          <View style={styles.containerCard}>
-            <Text style={styles.title}>Testing PieChart ⏺</Text>
-            <View style={styles.containerVerticalGraph}>
-              <PieChart
-                data={data}
-                keyLabel={"nameProduct"}
-                keyValue={"value"}
-                primaryKey={"id"}
-                error={false}
-                loading={false}
-              />
-            </View>
+          {/* With Scroll */}
+          <Text style={styles.title}>Internal scroll 📈</Text>
+          <View style={[styles.containerHorizontalGraph, { height: 150 }]}>
+            <MultipleHorizontalBars
+              data={data}
+              keyLabel={"nameProduct"}
+              keyValue={"value"}
+              primaryKey={"id"}
+              error={false}
+              loading={false}
+            />
           </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      )}
+
+      {/* MultipleVerticalBars */}
+      {type === "verticalBars" && (
+        <View style={styles.containerCard}>
+          {/* Without Scroll */}
+          <Text style={styles.title}>MultipleVerticalBars 📈</Text>
+          <View style={styles.containerVerticalGraph}>
+            <MultipleVerticalBars
+              data={data}
+              keyLabel={"nameProduct"}
+              keyValue={"value"}
+              primaryKey={"id"}
+              error={false}
+              loading={false}
+            />
+          </View>
+          {/* With Scroll */}
+          <Text style={styles.title}>Internal scroll 📈</Text>
+          <View style={[styles.containerVerticalGraph, { width: 200 }]}>
+            <MultipleVerticalBars
+              data={data}
+              keyLabel={"nameProduct"}
+              keyValue={"value"}
+              primaryKey={"id"}
+              error={false}
+              loading={false}
+            />
+          </View>
+        </View>
+      )}
+
+      {/* PIE CHART */}
+      {type === "pieChart" && (
+        <View style={styles.containerCard}>
+          <Text style={styles.title}>Testing PieChart ⏺</Text>
+          <View style={styles.containerPieGraph}>
+            <PieChart
+              data={data}
+              keyLabel={"nameProduct"}
+              keyValue={"value"}
+              primaryKey={"id"}
+              error={false}
+              loading={false}
+            />
+          </View>
+        </View>
+      )}
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: "column",
     backgroundColor: "#ececec",
     alignItems: "center",
-    paddingTop: 75,
+    paddingTop: 30,
     gap: 30,
     paddingHorizontal: 20,
-    paddingBottom: 75,
+    marginBottom: 75,
   },
   title: {
     fontSize: 16,
@@ -162,5 +246,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: 360,
     height: 275,
+  },
+  containerPieGraph: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 350,
+    height: 350,
   },
 });
